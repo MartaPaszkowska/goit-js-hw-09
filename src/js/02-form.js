518c43e066f40b8810b9fef82fb97190c2b9799b
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-07-11 14:19:40 +0200

    w

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -5,7 +5,6 @@ const message = form.elements.message;
 const storageKey = 'feedback-form-state';
 
 form.addEventListener('input', setLocalStorage);
-
 function setLocalStorage() {
   const formData = {
     email: email.value.trim(),
@@ -13,19 +12,30 @@ function setLocalStorage() {
   };
   localStorage.setItem(storageKey, JSON.stringify(formData));
 }
-
 form.addEventListener('submit', event => {
   event.preventDefault();
 
   if (email.value.trim() === '' || message.value.trim() === '') {
     alert('Please complete the form.');
+    return;
   }
-
+  function clearForm() {
+    localStorage.removeItem(storageKey);
+    form.reset();
+  }
+  function loadForm() {
+    const savedStorage = localStorage.getItem(storageKey);
+    if (savedStorage) {
+      const { email, message } = JSON.parse(savedStorage);
+      email.value = email || '';
+      message.value = message || '';
+    }
+  }
   const formData = {
     email: email.value.trim(),
     message: message.value.trim(),
   };
   console.log(formData);
-  localStorage.removeItem(storageKey);
-  form.reset();
+  clearForm();
+  loadForm();
 });
